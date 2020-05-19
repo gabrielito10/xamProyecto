c6da7fac899492adaf2a16b71225a9fc7268ded2
gabrielito10 <j.gabito.ic@gmail.com>
2020-05-19 18:24:34 -0400

    xamProyecto

---
+++ xamProyecto/xamProyecto/Infraestructura/InstanciaLocator.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using xamProyecto.ViewModels;
+
+namespace xamProyecto.Infraestructura
+{
+    class InstanciaLocator
+    {
+        public MainViewModel Main { get; set; }
+
+        public InstanciaLocator()
+        {
+            this.Main = new MainViewModel();
+        }
+    }
+}
---
+++ xamProyecto/xamProyecto/ViewModels/MainViewModel.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace xamProyecto.ViewModels
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MainViewModel : ContentPage
+    {
+        public MainViewModel()
+        {
+            InitializeComponent();
+        }
+    }
+}
