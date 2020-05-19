4c431220b473e842cbae37fbae3ebc9c5291fa1e
gabrielito10 <j.gabito.ic@gmail.com>
2020-05-19 17:03:57 -0400

    Login Page

--- xamProyecto/xamProyecto/App.xaml.cs
+++ xamProyecto/xamProyecto/App.xaml.cs
@@ -10,7 +10,7 @@ namespace xamProyecto
         {
             InitializeComponent();
 
-            MainPage = new MainPage();
+            MainPage = new ();
         }
 
         protected override void OnStart()
