debfb53fd2541267d08ab7cdd7148db551701b59
gabrielito10 <j.gabito.ic@gmail.com>
2020-05-19 18:41:46 -0400

    xamProyecto

---
+++ xamProyecto/xamProyecto/ViewModels/MainViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace xamProyecto.ViewModels
+{
+    public class MainViewModel
+    {
+        public LoginViewModel Login { get; set; }
+    }
+
+    #region Contructures
+    public MainViewModel()
+    {
+        this.lo
+    }
+    #endregion
+}
