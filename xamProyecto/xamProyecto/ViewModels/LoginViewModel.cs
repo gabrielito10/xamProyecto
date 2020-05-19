19e30c6c93792bc1eaa9594fc860cb14399ae937
gabrielito10 <j.gabito.ic@gmail.com>
2020-05-19 19:11:39 -0400

    xamProyecto

--- xamProyecto/xamProyecto/ViewModels/LoginViewModel.cs
+++ xamProyecto/xamProyecto/ViewModels/LoginViewModel.cs
@@ -1,10 +1,25 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Windows.Input;
 
 namespace xamProyecto.ViewModels
 {
     public class LoginViewModel
     {
+        #region Propiedades
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public bool EstaCorriendo { get; set; }
+        public bool EstaRecordando { get; set; }
+        public  ICommand ComandoLogin { get; set; }
+        #endregion
+
+        #region Contructores
+        public LoginViewModel()
+        {
+            this.EstaRecordando = true;
+        }
+        #endregion
     }
 }
--- xamProyecto/xamProyecto/ViewModels/MainViewModel.cs
+++ xamProyecto/xamProyecto/ViewModels/MainViewModel.cs
@@ -7,12 +7,12 @@ namespace xamProyecto.ViewModels
     public class MainViewModel
     {
         public LoginViewModel Login { get; set; }
-    }
 
-    #region Contructures
-    public MainViewModel()
-    {
-        this.lo
+        #region Contructures
+        public MainViewModel()
+        {
+            this.Login = new LoginViewModel();
+        }
+        #endregion
     }
-    #endregion
 }
