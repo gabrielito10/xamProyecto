ec36fe92d4b14fefce308bcbf124b03774f822c6
gabrielito10 <j.gabito.ic@gmail.com>
2020-05-20 22:36:26 -0400

    xamProyecto

---
+++ xamProyecto/xamProyecto/ViewModels/PaisItemViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace xamProyecto.ViewModels
+{
+    class PaisItemViewModel
+    {
+    }
+}
--- xamProyecto/xamProyecto/ViewModels/PaisesViewModel.cs
+++ xamProyecto/xamProyecto/ViewModels/PaisesViewModel.cs
@@ -1,6 +1,10 @@
 ﻿
+using GalaSoft.MvvmLight.Command;
+using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Linq;
+using System.Windows.Input;
 using Xamarin.Forms;
 using xamProyecto.Models;
 using xamProyecto.Services;
@@ -11,42 +15,89 @@ namespace xamProyecto.ViewModels
     public class PaisesViewModel: BaseViewModel
     {
         private ObservableCollection<ListaPaises> _Paises;
-        private ApiService ServicioAPI;
+        private ApiService _ServicioAPI;
+        private bool _EstaRefrescando;
+        private string _Filtro;
+        private List<ListaPaises> _ListaPaises;
         #region Propiedades
         public ObservableCollection<ListaPaises> Paises
         {
             get { return this._Paises; }
             set { SetValue(ref this._Paises, value); }
         }
+
+        public bool EstaRefrescando
+        {
+            get { return this._EstaRefrescando; }
+            set { SetValue(ref this._EstaRefrescando, value);}
+        }
+
+        public string  Filtro
+        {
+            get { return this._Filtro; }
+            set 
+            { 
+                SetValue(ref this._Filtro, value);
+                this.Buscar();
+            }
+        }
         #endregion
         #region Contructores
-      
+
         public PaisesViewModel()
         {
-            this.ServicioAPI = new ApiService();
+            this._ServicioAPI = new ApiService();
             this.CargarPaises();
         }
         #endregion
         #region Metodos
         private async void CargarPaises()
         {
-            var conexion = await this.ServicioAPI.CheckConnection();
+            this.EstaRefrescando = true;
+            var conexion = await this._ServicioAPI.CheckConnection();
             if(!conexion.IsSuccess)
             {
+                this.EstaRefrescando = false;
                 await Application.Current.MainPage.DisplayAlert("Error", conexion.Message, "Aceptar");
                 await Application.Current.MainPage.Navigation.PopAsync();
                 return;
             }
-            var Response = await this.ServicioAPI.GetList<ListaPaises>("http://restcountries.eu", "/rest", "/v2/all");
+            var Response = await this._ServicioAPI.GetList<ListaPaises>("http://restcountries.eu", "/rest", "/v2/all");
             if (!Response.IsSuccess)
             {
                 await Application.Current.MainPage.DisplayAlert("Error", Response.Message, "Aceptar");
                 return;
                 
             }
-            var lista = (List<ListaPaises>)Response.Result;
-            this.Paises = new ObservableCollection<ListaPaises>(lista);
+            this._ListaPaises= (List<ListaPaises>)Response.Result;
+            this.Paises = new ObservableCollection<ListaPaises>(_ListaPaises);
+            this.EstaRefrescando = false;
         }
         #endregion
+
+        public ICommand ComandoRefrescar
+        {
+            get
+            {
+                return new RelayCommand(CargarPaises);
+            }
+        }
+        public ICommand ComandoBuscar
+        {
+            get
+            {
+                return new RelayCommand(Buscar);
+            }
+        }
+
+        private void Buscar()
+        {
+            if (string.IsNullOrEmpty(this._Filtro))
+                this.Paises = new ObservableCollection<ListaPaises>(this._ListaPaises);
+            else
+                this.Paises = new ObservableCollection<ListaPaises>(this._ListaPaises.Where(
+                    l => l.Name.ToLower().Contains(this._Filtro.ToLower())||
+                    l.Capital.ToLower().Contains(this._Filtro.ToLower())));
+        }
     }
 }
